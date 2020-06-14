89c00733444cbfe23b75ff869726429f5799cee6
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-14 13:34:51 +0530

    Initial project setup

---
+++ GetWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetWeather
+//
+//  Created by Anuranjan Bose on 14/06/20.
+//  Copyright © 2020 Anuranjan Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
