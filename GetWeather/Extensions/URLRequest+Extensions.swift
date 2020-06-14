a3e15e5c99ff5fdc617719f9feb231863098ef37
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-14 14:06:48 +0530

    Added URLRequest extension

---
+++ GetWeather/Extensions/URLRequest+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  URLRequest+Extensions.swift
+//  GetWeather
+//
+//  Created by Anuranjan Bose on 14/06/20.
+//  Copyright © 2020 Anuranjan Bose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+struct Resource<T> {
+    let url: URL
+}
+
+extension URLRequest {
+    
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        return Observable.from([resource.url])
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+        }.map { data -> T in
+            return try JSONDecoder().decode(T.self, from: data)
+        }.asObservable()
+    }
+}
+
